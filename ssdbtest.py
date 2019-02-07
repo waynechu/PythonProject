eae2bd38a2b9bd55564c24b84bab326b10c57730
waynechu <waynechu.twn@gmail.com>
2019-02-08 06:42:41 +0800

    revise

--- ssdbtest.py
+++ ssdbtest.py
@@ -6,68 +6,60 @@ from pyssdb import pyssdb
 
 def parameter_check(argv):
 
-    Parameters = {"ConfigFile": ""}
+    parameters = {"ConfigFile": ""}
     idx, argc = 0, len(argv)
 
     while idx < argc:
         if (sys.argv[idx] == "-f") and (idx < argc - 1):
             idx = idx + 1
-            Parameters["ConfigFile"] = sys.argv[idx]
+            parameters["ConfigFile"] = sys.argv[idx]
         idx = idx + 1
 
-    if (Parameters["ConfigFile"] == ""):
-        print("-f <config_file_name>")
+    if (parameters["ConfigFile"] == ""):
+        print("python ssdbtest.py -f <config_file_name>")
         exit(0)
     else:
-        return Parameters
+        return parameters
+
+def load_config(filename):
+
+    fd = open(filename)
+    data = fd.read()
+    fd.close()
+
+    return data
 
 if __name__ == '__main__':
 
-    Parameters = parameter_check(sys.argv)
+    parameters = parameter_check(sys.argv)
 
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s-%(thread)06d-%(levelname)s: %(message)s", datefmt="%Y%m%d-%H%M%S")
 
-    ConfFD = open(Parameters["ConfigFile"])
-    ConfData = ConfFD.read()
-    ConfFD.close()
-    print(ConfData)
+    confData = load_config(parameters["ConfigFile"])
+    agentConf = json.loads(confData)
+    ssdbHost = agentConf["SSDB"]["Host"]
+    ssdbPort = agentConf["SSDB"]["Port"]
+    ssdbPasscode = agentConf["SSDB"]["Passcode"]
 
-    AgentConf = json.loads(ConfData)
-    SSDBHost = AgentConf["SSDB"]["Host"]
-    SSDBPort = AgentConf["SSDB"]["Port"]
-    SSDBPasscode = AgentConf["SSDB"]["Passcode"]
+    confSSDB = pyssdb.Client(host = ssdbHost, port = ssdbPort)
 
-    ConfSSDB = pyssdb.Client(host = SSDBHost, port = SSDBPort)
+    confSSDB.auth(ssdbPasscode)
 
-    ConfSSDB.auth(SSDBPasscode)
+    names = confSSDB.hlist("", "", 100)
 
-    #print(ConfSSDB.hgetall("DNS-Agent-Sync"), end = "\n\n")
-    #print(ConfSSDB.hkeys("DNS-Agent-Sync", "", "", 100), end = "\n\n")
-    #print(ConfSSDB.hgetall("DNS-Agent-Status"), end = "\n\n")
-    #print(ConfSSDB.hkeys("DNS-Agent-Status", "", "", 100), end = "\n\n")
+    for name in names:
+        logging.info(name.decode("utf-8"))
 
-    Names = ConfSSDB.hlist("", "", 100)
+    logging.info("------------------------------")
 
-    for Name in Names:
-        print(Name)
+    statusSize = confSSDB.hsize("DNS-Agent-Sync")
+    statusHashes = confSSDB.hkeys("DNS-Agent-Sync", "", "", statusSize)
 
-    print("------------------------------")
-
-    #LogSize = ConfSSDB.hsize("DNS-Agent-Log-HKSGDNSDF01")
-    #hksgdnsdf01 = ConfSSDB.hgetall("DNS-Agent-Log-HKSGDNSDF01", "", "", LogSize)
-    #for log in hksgdnsdf01:
-    #    print(log)
-
-    print("------------------------------")
-
-    StatusSize = ConfSSDB.hsize("DNS-Agent-Sync")
-    StatusHashes = ConfSSDB.hkeys("DNS-Agent-Sync", "", "", StatusSize)
-
-    for Agent in StatusHashes:
-        Status = ConfSSDB.hget("DNS-Agent-Sync", Agent)
-        if Status == b"Sync":
-            print(Agent, "is good!")
+    for agent in statusHashes:
+        status = confSSDB.hget("DNS-Agent-Sync", agent)
+        if status == b"Sync":
+            logging.info("%s is good", agent.decode("utf-8"))
         else:
-            print(Agent, "in trouble!")
+            logging.info("%s is in trouble!!", agent.decode("utf-8"))
 
-    ConfSSDB.disconnect()
+    confSSDB.disconnect()
