dc68514110d10d98361df2230102132bc2e413aa
waynechu <waynechu.twn@gmail.com>
2019-03-31 21:00:07 +0800

    smtp

--- smtptest.py
+++ smtptest.py
@@ -1,27 +1,11 @@
 import smtplib
 
-def prompt(prompt):
-    return input(prompt).strip()
+gmail_user = 'wayne.chu@nexusguard.com'  
+gmail_password = ''
 
-fromaddr = prompt("From: ")
-toaddrs  = prompt("To: ").split()
-print("Enter message, end with ^D (Unix) or ^Z (Windows):")
-
-# Add the From: and To: headers at the start!
-msg = ("From: %s\r\nTo: %s\r\n\r\n"
-       % (fromaddr, ", ".join(toaddrs)))
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if not line:
-        break
-    msg = msg + line
-
-print("Message length is", len(msg))
-
-server = smtplib.SMTP('localhost')
-server.set_debuglevel(1)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+try:  
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+except:  
+    print('Something went wrong...')
